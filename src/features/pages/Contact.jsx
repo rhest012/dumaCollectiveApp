cb29b9f00fe46fe7dedcb32afe881e058a37f6fd
Kagiso Semenya <kagiso@rhest.studio>
2023-06-25 19:46:54 +0200

    Contact Page update

--- src/features/pages/Contact.jsx
+++ src/features/pages/Contact.jsx
@@ -1,11 +1,15 @@
-import { Flex, Grid, GridItem, Heading, Input, Select, Stack, Text, Textarea } from '@chakra-ui/react'
+import { Flex, Grid, GridItem, Heading, Image, Input, Select, Stack, Text, Textarea } from '@chakra-ui/react'
 import SecondaryHeader from '../components/SecondaryHeader'
+import PlaceholderImageLs from "../../assets/bodyImages/DumaImage01.jpg"
+import PlaceholderImageP from "../../assets/bodyImages/DumaImage02.jpg"
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
+// Icons
 import {SlLocationPin} from 'react-icons/sl'
 import {BsEnvelopeAt, BsTelephone} from 'react-icons/bs'
 import {AiOutlineTwitter, AiOutlineInstagram} from 'react-icons/ai'
-import {FaFacebookF} from 'react-icons/fa'
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import {FaFacebookF, FaTiktok, FaLinkedinIn} from 'react-icons/fa'
 
 const Contact = () => {
     const inputStyling= {
@@ -19,6 +23,26 @@ const Contact = () => {
         }
     }
 
+    const imageStyling = {
+        height: "300px",
+        borderRadius: "0.5rem"
+    }
+
+    const singleGridContainerStyling = {
+        display:"flex", 
+        flexDir:"column",
+        height:"100%",
+        justifyContent:"center",
+        alignContent:"center"
+    }
+
+    const socialmediaContainerStyling = {
+        display:"flex",
+        flexDir:"column",
+        alignItems:"center"
+    }
+
+
         // Page Location
     const location = useLocation();
     const pageLocation = location.pathname;
@@ -27,125 +51,124 @@ const Contact = () => {
     <>
         <SecondaryHeader pageLocation={pageLocation}/>
         <Grid
-            marginY="4rem"
-            marginX="10rem"
-            gridTemplateColumns="70% 30%"
+            // marginY="4rem"
+            marginX="2rem"
+            marginY="8rem"
+            gridTemplateColumns="50% 50%"
+            gap="2rem"
         >
             <GridItem
-                borderRadius="1rem"
-                border="1px solid #1E174B"
-                padding="2rem"
-                height="500px"
-                zIndex="1"
+               sx={singleGridContainerStyling}
+               paddingBottom="2rem"
             >
-                <Flex height="100%" flexDirection="column" flexWrap="wrap" justifyContent="center">
-                    <Heading 
-                        variant="h3" 
-                        as="h3"
-                        color="brand.900"
-                        paddingBottom="1rem"
-                    >
-                        Send Us a Message
-                    </Heading>
-                    <form>
-                        <Flex gap="1rem" width="65%">
-                            <Input
-                                placeholder="Full Name"
-                                type="text"
-                                sx={inputStyling}
-                            />
-                            <Input
-                                placeholder="Email"
-                                type="email"
-                                sx={inputStyling}
-                            />
-                        </Flex>
-                        <Select
+                
+                <Heading 
+                    variant="h3" 
+                    as="h3"
+                >
+                    Questions?
+                </Heading>
+                <Text 
+                    variant="p" 
+                    paddingBottom="2rem"
+                >
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus rem blanditiis nobis laborum molestiae neque dicta nihil quibusdam. Sed, dolorem.
+                </Text>
+                <Flex justifyContent="space-between">
+                    <Image
+                        src={PlaceholderImageLs}
+                        sx={imageStyling}
+                    />
+                    <Image
+                        src={PlaceholderImageP}
+                        sx={imageStyling}
+                    />
+                </Flex>
+            </GridItem>
+            <GridItem
+                sx={singleGridContainerStyling}
+                marginRight="2rem"
+            >
+                <form>
+                    <Flex gap="1rem">
+                        <Input
+                            placeholder="Full Name"
+                            type="text"
                             sx={inputStyling}
-                            placeholder="Subject"
-                            width="65%"
-                        >
-                            <option value='business'>Business</option>
-                            <option value='careers'>Careers</option>
-                            <option value='enquiries'>General Enquiries</option>
-                        </Select>
-                        <Textarea
-                            placeholder="Message"
-                            sx={inputStyling}
-                            width="65%"
                         />
-                    </form>
+                        <Input
+                            placeholder="Email"
+                            type="email"
+                            sx={inputStyling}
+                        />
+                    </Flex>
+                    <Select
+                        sx={inputStyling}
+                        placeholder="Subject"
+                    >
+                        <option value='business'>Business</option>
+                        <option value='careers'>Careers</option>
+                        <option value='enquiries'>General Enquiries</option>
+                    </Select>
+                    <Textarea
+                        placeholder="Message"
+                        sx={inputStyling}
+                    />
+                </form>
+                <Grid gridTemplateColumns="repeat(3, 1fr)" justifyContent="center" paddingY="1rem">
+                    <GridItem sx={socialmediaContainerStyling}>
+                        <SlLocationPin
+                            className="contact-icon"
+                        />
+                        <Text variant="p" textAlign="center">
+                            51 Glenhove, Melrose Estate Johannesburg, 2196
+                        </Text>
+                    </GridItem>
+                    <GridItem sx={socialmediaContainerStyling}>
+                        <BsEnvelopeAt
+                            className="contact-icon"
+                        />
+                        <Text variant="p">
+                            info@dumacollective.com
+                        </Text>
+                    </GridItem>
+                    <GridItem sx={socialmediaContainerStyling}>
+                        <BsTelephone
+                            className="contact-icon"
+                        />
+                        <Text variant="p">
+                            0104435854​
+                        </Text>
+                    </GridItem>
+                </Grid>
+                <Flex flexDirecion="column" paddingY="1rem" justifyContent="space-between">
+                    <Link>
+                        <FaFacebookF
+                            className="contact-icon"
+                        />
+                    </Link>
+                    <Link>
+                        <FaTiktok
+                            className="contact-icon"
+                        />
+                    </Link>
+                    <Link>
+                        <FaLinkedinIn
+                            className="contact-icon"
+                        />
+                    </Link>
+                    <Link>
+                        <AiOutlineTwitter
+                            className="contact-icon"
+                        />
+                    </Link>
+                    <Link>
+                        <AiOutlineInstagram
+                            className="contact-icon"
+                        />
+                    </Link>
                 </Flex>
-        </GridItem>
-        <GridItem
-            marginTop="6rem"
-            marginLeft="-200px"
-            borderRadius="1rem"
-            background="brand.900"
-            padding="2rem"
-            width="400px"
-            height="500px"
-            zIndex="2"
-            
-        >
-            <Flex height="100%" flexDir="column" gap="2rem" flexWrap="wrap" alignContent="center" justifyContent="center">
-            <Heading 
-                variant="h3" 
-                as="h3"
-                color="#fff"
-                paddingBottom="0.5rem"
-            >
-                Contact Us
-            </Heading>
-                <Flex
-                    gap="1rem"
-                >
-                    <SlLocationPin
-                        color="#fff"
-                        fontSize="1.25rem"
-                    />
-                    <Text variant="p" color="#fff">
-                        51 Glenhove, Melrose Estate Johannesburg, 2196
-                    </Text>
-                </Flex>
-                <Flex
-                    gap="1rem"
-                >
-                    <BsEnvelopeAt
-                        color="#fff"
-                        fontSize="1.25rem"
-                    />
-                    <Text variant="p" color="#fff">
-                        info@dumacollective.com
-                    </Text>
-                </Flex>
-                <Flex
-                    gap="1rem"
-                >
-                    <BsTelephone
-                        color="#fff"
-                        fontSize="1.25rem"
-                    />
-                    <Text variant="p" color="#fff">
-                        0104435854​
-                    </Text>
-                </Flex>
-                <Flex flexDirecion="column" gap="5rem" marginTop="1rem">
-                    <FaFacebookF
-                        color="#fff"
-                        fontSize="1.5rem"
-                    />
-                    <AiOutlineTwitter
-                        color="#fff"
-                        fontSize="1.5rem"
-                    />
-                    <AiOutlineInstagram
-                        color="#fff"
-                        fontSize="1.5rem"
-                    />
-                </Flex>
-            </Flex>
-        </GridItem>
+        </GridItem>  
       </Grid>
     </>
   )
