04746d93ac2332816227be668e8201c98588bd22
Kagiso Semenya <kagiso@rhest.studio>
2023-07-05 12:04:11 +0200

    Get In Touch link fix

--- src/App.js
+++ src/App.js
@@ -5,17 +5,6 @@ import Nav from './components/Nav';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import CustomCursor from './components/CustomCursor';
 
-// Pages and components
-import Jff from './features/theGrind/pages/Jff';
-import Squad from './features/pages/Squad';
-import NewsRoom from './features/theNewsRoom/pages/NewsRoom';
-import TheGrind from './features/theGrind/pages/TheGrind'
-import Home from './features/pages/Home'
-import WhatWeDo from './features/pages/WhatWeDo';
-import Contact from './features/pages/Contact';
-import SingleService from './features/pages/SingleService';
-import Loader from './features/components/Loader';
-import Footer from './components/Footer';
 import AnimatedRoutes from './features/components/AnimatedRoutes';
 
 function App() {
