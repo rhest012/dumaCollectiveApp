f36402a39877b853e18c9fb67f798007418815b7
Kagiso Semenya <kagiso@rhest.studio>
2023-09-13 18:11:14 +0200

    Video Player

--- src/features/components/HomeShowreel.jsx
+++ src/features/components/HomeShowreel.jsx
@@ -53,7 +53,7 @@ const HomeShowreel = () => {
         justifyContent="center"
         width="100%"
         height="100%"
-        paddingY={{ base: "2rem", lg: "0" }}
+
         // marginTop={{ base: "2rem", sm: "2rem", md: "2rem", lg: "0", xl: "0" }}
       >
         <ReactPlayer
