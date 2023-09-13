2903f8920adde9ab01bcb16f18e1aba66df8793b
Kagiso Semenya <kagiso@rhest.studio>
2023-09-13 17:20:35 +0200

    Video

--- src/features/components/HomeShowreel.jsx
+++ src/features/components/HomeShowreel.jsx
@@ -1,4 +1,12 @@
-import { Button, Grid, GridItem, Heading, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import Video from "../../assets/videos/homeVideo.mp4";
 import PlaceHolderImage from "../../assets/headers/newsroom-header.jpg";
 import React from "react";
@@ -12,8 +20,8 @@ const HomeShowreel = () => {
         base: "1fr",
         sm: "1fr",
         md: "1fr",
-        lg: "1fr 2fr",
-        xl: "1fr 2fr",
+        lg: "40% 60%",
+        xl: "40% 60%",
       }}
       height={{ base: "auto", sm: "auto", md: "auto", lg: "80vh", xl: "90vh" }}
       overflow="hidden"
@@ -43,18 +51,19 @@ const HomeShowreel = () => {
         alignContent="center"
         alignItems="center"
         justifyContent="center"
-        marginTop={{ base: "2rem", sm: "2rem", md: "2rem", lg: "0", xl: "0" }}
+        // marginTop={{ base: "2rem", sm: "2rem", md: "2rem", lg: "0", xl: "0" }}
       >
-        {/* <ReactPlayer
-          width="400px"
-          height="711px"
-          paddingY="4rem"
-          url={Video}
-          playing={true}
-          loop={true}
-          muted={true}
-        /> */}
-        <Image src={PlaceHolderImage} height="500px" />
+        <Box height="100%" width="100%" paddingY="2rem">
+          <ReactPlayer
+            url="https://vimeo.com/864014782?share=copy"
+            playing={true}
+            loop={true}
+            muted={true}
+            width="100%"
+            // height={{ base: "inherit", lg: "100%" }}
+          />
+        </Box>
+        {/* <Image src={PlaceHolderImage} height="500px" /> */}
       </GridItem>
     </Grid>
   );
