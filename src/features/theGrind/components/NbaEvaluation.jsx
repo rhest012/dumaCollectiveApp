65dbbdc50b097baa9ba6cffcb6c3856f1f40a6d7
Kagiso Semenya <kagiso@rhest.studio>
2023-09-20 12:21:15 +0200

    NBA Case Study

--- src/features/theGrind/components/NbaEvaluation.jsx
+++ src/features/theGrind/components/NbaEvaluation.jsx
@@ -19,42 +19,107 @@ const NbaEvaluation = () => {
     display: "inline-block",
     borderRadius: "2rem",
   };
+
+  const inlineHeading = {
+    marginLeft: "1rem",
+    variant: "h3",
+    color: "#fff",
+  };
   return (
-    <Box
-      margin="4rem 2rem"
-      backgroundImage={NbaImage1}
-      height="100%"
-      backgroundPosition="center"
-      backgroundSize="contain"
-    >
-      <Box padding=" 4rem 2rem" width="70%">
-        <Heading sx={socialContainerHeading}>Evaluation</Heading>
-        <Box>
-          <Text fontSize="1.25rem" variant="p" color="#fff">
-            A total of three prominent influencers took center stage in the
-            campaign, collectively delivering an impressive impact. The campaign
-            witnessed a staggering 74 posts shared by these influencers,
-            captivating an extensive audience of
-            <Heading marginLeft="1rem" variant="h3" color="#fff" as="span">
-              16 million individuals.
-            </Heading>
-          </Text>
+    <>
+      <Box
+        margin="4rem 2rem"
+        backgroundImage={NbaImage1}
+        height="550px"
+        backgroundPosition="center"
+        backgroundSize="contain"
+      >
+        <Box padding=" 4rem 2rem" width="70%">
+          <Heading sx={socialContainerHeading}>Evaluation</Heading>
+          <Box>
+            <Text fontSize="1.25rem" variant="p" color="#fff">
+              A total of three prominent influencers took center stage in the
+              campaign, collectively delivering an impressive impact. The
+              campaign witnessed a staggering 74 posts shared by these
+              influencers, captivating an extensive audience of
+              <Heading marginLeft="1rem" variant="h3" color="#fff" as="span">
+                16 million individuals.
+              </Heading>
+            </Text>
+          </Box>
+          <Box>
+            <Text fontSize="1.25rem" variant="p" color="#fff" marginY="2rem">
+              The engagement levels soared with a remarkable total of
+              <Heading sx={inlineHeading} as="span">
+                2.7 million interactions
+              </Heading>
+              , resulting in an outstanding
+              <Heading sx={inlineHeading} as="span">
+                engagement rate of 16.2%
+              </Heading>
+            </Text>
+          </Box>
+          <Box>
+            <Text fontSize="1.25rem" variant="p" color="#fff" marginY="2rem">
+              These impressive statistics reflect the tremendous success
+              achieved in driving awareness, fostering connections, and sparking
+              meaningful conversations surrounding the campaign.
+            </Text>
+          </Box>
         </Box>
-        <Text fontSize="1.25rem" variant="p" color="#fff" marginY="2rem">
-          The engagement levels soared with a remarkable total of
-          <Heading variant="h3" color="#fff" as="span" marginLeft="1rem">
-            2.7 million interactions
-          </Heading>
-          , resulting in an outstanding
-          <Heading variant="h3" color="#fff" as="span" marginX="1rem">
-            engagement rate of 16.2%
-          </Heading>
-          These impressive statistics reflect the tremendous success achieved in
-          driving awareness, fostering connections, and sparking meaningful
-          conversations surrounding the campaign.
-        </Text>
       </Box>
-    </Box>
+
+      <Box
+        margin="4rem 2rem"
+        backgroundImage={NbaImage1}
+        height="550px"
+        backgroundPosition="center"
+        backgroundSize="contain"
+      >
+        <Box padding=" 4rem 2rem" width="70%">
+          <Heading sx={socialContainerHeading}>The Results</Heading>
+          <Box>
+            <Text fontSize="1.25rem" variant="p" color="#fff">
+              Leveraging a powerful combination of social media, PR, and
+              influencer partnerships, our efforts yielded remarkable results,
+              making a lasting impact
+              <Heading marginLeft="1rem" variant="h3" color="#fff" as="span">
+                4,802,522 individuals were reached
+              </Heading>
+            </Text>
+          </Box>
+          <Box>
+            <Text fontSize="1.25rem" variant="p" color="#fff" marginY="2rem">
+              The campaign generated an impressive
+              <Heading variant="h3" color="#fff" as="span" marginLeft="1rem">
+                1.3 million interactions
+              </Heading>
+              , showcasing the depth of audience connection and enthusiasm.
+            </Text>
+          </Box>
+          <Box>
+            <Text fontSize="1.25rem" variant="p" color="#fff" marginY="2rem">
+              With an impressive
+              <Heading variant="h3" color="#fff" as="span" marginLeft="1rem">
+                1.3 million impressions
+              </Heading>
+              , our message resonated across diverse platforms, leaving an
+              indelible mark.
+            </Text>
+          </Box>
+          <Box>
+            <Text fontSize="1.25rem" variant="p" color="#fff" marginY="2rem">
+              Encouragingly, our content drove a remarkable
+              <Heading variant="h3" color="#fff" as="span" marginLeft="1rem">
+                380,982 clicks
+              </Heading>
+              , demonstrating the significant interest and desire for further
+              engagement.
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+    </>
   );
 };
 
