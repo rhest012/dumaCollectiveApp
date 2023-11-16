b5f9e4704d2c37a0a2f35ff2d4acb703f6a46560
Kagiso Semenya <kagiso@rhest.studio>
2023-11-16 17:09:39 +0200

    css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Nav from './components/Nav';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import CustomCursor from './components/CustomCursor';
 
 import AnimatedRoutes from './features/components/AnimatedRoutes';
 
